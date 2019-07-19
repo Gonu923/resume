3481f2a4b36e696eda47d2bccc1f7b11bd7025e0
Brand Traversy <gonesh923@gmail.com>
2019-07-19 23:53:04 +0600

    Updates

---
+++ precache-manifest.7c0781b30a838b65895ec1eb27fb87dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5861f1771c8f35af93c86cd149a38ae8",
+    "url": "/Gonu923/resume/index.html"
+  },
+  {
+    "revision": "4741d26e448b3e182cc9",
+    "url": "/Gonu923/resume/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7cf49eb0593c46af349c",
+    "url": "/Gonu923/resume/static/css/main.03731e84.chunk.css"
+  },
+  {
+    "revision": "4741d26e448b3e182cc9",
+    "url": "/Gonu923/resume/static/js/2.191508db.chunk.js"
+  },
+  {
+    "revision": "7cf49eb0593c46af349c",
+    "url": "/Gonu923/resume/static/js/main.0f950f57.chunk.js"
+  },
+  {
+    "revision": "1f0e0b4e7fb9902374e4",
+    "url": "/Gonu923/resume/static/js/runtime~main.e553e6c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Gonu923/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
